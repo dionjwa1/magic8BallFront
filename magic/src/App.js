6ab7da4253eb5bdfff83bdb6ecc1448b331cf8c4
dionjwa1 <dionjwa@gmail.com>
2021-07-18 17:48:01 -0700

    update

--- magic/src/App.js
+++ magic/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {Container, Row, Col}  from 'react-bootstrap';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Dion's Magic App...Coming Soon!
-        </a>
-      </header>
-    </div>
-  );
+  <>
+<Container fluid="md">
+  <Row>
+    <Col>Dion Magic</Col>
+  </Row>
+</Container>
+ 
+Dion's Magic App...Coming Soon!
+</> );
+
 }
 
 export default App;
